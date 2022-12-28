fce0c94fed98dd7f72001d6cadacae88ad09998a
MoustafaAshraf8 <mostafa.ashraf8642@gmail.com>
2022-12-28 16:53:21 +0200

    sic class

---
+++ Class/SICclass.py
@@ -0,0 +1,12 @@
+import pandas as pd
+class Program_ABSOLUTE:
+   df: pd.DataFrame
+   DFlist: list
+   LoadMap: pd.DataFrame
+   MEMORY: pd.DataFrame
+
+   def __init__(self):
+      return
+
+   def printdf(self):
+      print(self.df)
