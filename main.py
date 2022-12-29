7ffd4dafc186e484662efb178b156c2d70f04032
MoustafaAshraf8 <mostafa.ashraf8642@gmail.com>
2022-12-29 09:26:15 +0200

    main handling

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import pandas as pd
 import numpy as np
-import os
-import glob
+import SIC.AbsoluteLoader as abs
 import SIC.SICparser as SICparser
 import SIC.SICloadmap as SICloadmap
 import SIC.SICmemory as SICmemory
 import SIC.SICmemoryFill as SICmemoryFill
+import SICXE.SICXEparser as parserSICXE
+import SICXE.SICXEloadMap as LM
+import SICXE.SICXEmemory as MEM
+import SICXE.SICXEmemoryFill as MEMfill
+import Class.SICclass as sic
+import Class.SICXEClass as sicxe
 from pandasgui import show
-
 
 Program_type = input("Enter (1) for Absolute loader and (2) for Linker-loader:   ")
 if(Program_type == "1"):
-   files = glob.glob('./SIC/Res/*')    # clear result dir for new files
-   for q in files:
-     os.remove(q)
    prog = SICparser.parseFile("Test/sic.txt")
    prog = SICloadmap.loadMap(prog=prog,Launch="4000")
    prog = SICmemory.memorySetup(prog=prog)
    prog = SICmemoryFill.memoryFill(prog=prog)
-   prog.MemoryShow(prog)
+   show(prog.MEMORY)
+
+else:
+   startAddress = input("Enter address to start from:    ")
+   prog = parserSICXE.parseFile(FilePath="Test/sicxe.txt")
+   #start = input("enter the start address: ")
+   prog = LM.loadMap(prog=prog,Launch="4000")
+   prog = MEM.memorySetup(prog=prog)
+   prog = MEMfill.memoryFill(prog=prog)
+   show(prog.MEMORY)
+
+
+
+
